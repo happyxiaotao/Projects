4b9e33eb1d461598a350e38511ea45a250cae669
石俊涛 <1550637676@qq.com>
2017-04-11 23:47:50 +0800

    C++ projects

---
+++ C++/解析new与delete/解析new与delete/test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "new_delete.h"
+
+using namespace std;
+
+class Array
+{
+public :
+	Array();
+	const int & operator[](int idex) const;
+	int& operator[] (int idex);
+private :
+	int* array;
+};
+
+Array::Array():array(new int[10])
+{
+	for (int idex = 0; idex < 10; ++idex)
+		array[idex] = idex;
+}
+const int& Array::operator[] (int idex)const
+{
+	return array[idex];
+}
+int& Array::operator[] (int idex)
+{
+	return array[idex];
+}
+
+int main()
+{
+	const Array a1;
+
+	cout << a1[2] << endl;
+	system("pause");
+	return 0;
+}
