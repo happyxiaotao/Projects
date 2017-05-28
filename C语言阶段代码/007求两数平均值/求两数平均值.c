5ac5c18c7faa7ca70b1179970ec2a858395d25f7
石俊涛 <1550637676@qq.com>
2017-05-28 21:24:49 +0800

    求平均值

--- C语言阶段代码/007求两数平均值/求两数平均值.c
+++ C语言阶段代码/007求两数平均值/求两数平均值.c
@@ -6,7 +6,9 @@ int main()
 
 	int average1 = b - ((a - b)>>1);//不能使用a - ((a-b)>>1),求得的值会大1
 	int average2 = (a & b) + ((a ^ b)>>1);
+	int average3 = (a + b) >> 1;
 
 	printf("a = %d, b = %d, average = b + ((a - b)>>1) = %d\n",a, b, average1);
 	printf("a = %d, b = %d, average = (a & b) + ((a ^ b)>>1) = %d\n",a, b, average2);
+	printf("a = %d, b = %d, average = (a + b) >> 1 = %d\n",a, b, average3);
 }
